3318dc944c05160671bcac516286e189de5ce6a8
snitolya <maslop@bk.ru>
2021-12-28 21:15:24 +0300

    fr

---
+++ 28.12/js/main.js
@@ -0,0 +1,55 @@
+console.log ('Silence is golden')
+console.log ('вторник')
+console.log ('Оля')
+console.log ('декабрь')
+
+let a = 1 ;
+let b = 2 ;
+let c = 4 ;
+alert( a + b -4) ;
+
+alert( a/b + a/c);
+
+a=2;
+b=3;
+alert( (a + 4*b)*(a-3*b)+a**2);
+
+let first = +prompt ('Введите первое число');
+let second = +prompt ('Введите второе число');
+let s = first + second;
+alert ( first + second );
+
+
+
+let f = +prompt('напиши число');
+
+if (f > 3 ){
+    f = f+10;
+} else {
+    f = f-10;
+}
+alert (f);
+
+
+let k = prompt('напиши число');
+
+if (k < 7 ){
+    alert ('Yes');
+} else if ( k > 10) {
+    alert ('No');   
+} else if (k == 9) {
+    alert ('Error');
+}
+
+
+
+let n = +prompt('ВВеди цифру')
+if (-10 < n < 10){
+    n = n+5;
+}
+else {
+    n = n-10;
+}
+alert (n);
+
+
