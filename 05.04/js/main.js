ea8b9c9401609bf050656d05d55b3a942819f781
snitolya <92542355+snitolya@users.noreply.github.com>
2022-04-06 21:01:19 +0300

    Update main.js

--- 05.04/js/main.js
+++ 05.04/js/main.js
@@ -1,65 +1,58 @@
-//Class Figure => point {х,у}
-//Class Rectangle => a,b
-//Class Square =>a
-//Circle=>r
-//all=> draw (х,у => ) / area , perimeter / name 
-
-
-
+// Сlass Figure => point{x,y}
+// Class Rectangle => a, b
+// Class Square => a
+// Circle => r
+// all => draw (x,y => ) | area, perimeter | name
+ 
 class Figure {
-    constructor(x,y) { 
+    constructor(x, y) {
         this.name = 'Figure';
-        this.x=x;
-        this.y=y;
+        this.x = x;
+        this.y = y;
     }
-   draw(){
-       console.log(`${this.name} x:${this.x}, y:${this}`)
-   } 
-   area(){
-       return 0;
-   }
- perimeter() {
-     return 0;
-   }
-    
-  }
-class Rectangle extends Figure {
-    constructor(x,y,a,b) {
+    draw(){
+        console.log(`${this.name} x: ${this.x}, y: ${this.y}`)
+    }
+    area(){
+        return 0;
+    }
+    perimeter(){
+        return 0;
+    }
+}
+class Rectangle extends Figure{
+    constructor(x, y, a, b) {
+        super(x,y);
         this.name = 'Rectangle';
-        super(x,y)
-        this.a=a;
-        this.b=b;
+        this.a = a;
+        this.b = b;
     }
     area(){
         return this.a * this.b;
     }
     perimeter(){
-        return (this.a + this.b)
+        return (this.a+this.b)*2;
     }
 }
-      
-class Square extends Rectangle {
-    constructor(x,y, a) {
+class Square extends Rectangle{
+    constructor(x, y, a) {
+        super(x, y, a, a);
         this.name = 'Square';
-        super(x,y, a, a)
-          }
+    }
 }
-
-class Circle extends Figure {
-    constructor(x,y , r) {
-        this.name = 'Circle';
-        super(x,y)
-      this.r=r;
+class Circle extends Figure{
+    constructor(x, y, r) {
+        super(x, y);
+        this.name = 'Circle'
+        this.r = r
     }
     area(){
         return Math.PI*this.r**2;
     }
     perimeter(){
-        return 2*Math
+        return 2*Math.PI*this.r;
     }
 }
-
-
-
-const figures = [new Figure (1,2), new Rectangle(1,2,4,8) , new Square (1,2,12)]
-figures.forEach(f=>console.log(f.area()));
+ 
+const figures = [new Figure(1,2), new Rectangle(1,2, 4,8), new Square(1,2, 12), new Circle(1,2, 4)]
+figures.forEach(f => console.log(f.area()));
